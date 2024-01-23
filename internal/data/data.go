d4245953c5135591b5a410632fce1e7f1fd8f7c0
Liu <bobo90326@163.com>
2024-01-24 02:18:48 +0800

    数据库接入
    数据库接入

--- internal/data/data.go
+++ internal/data/data.go
@@ -10,23 +10,24 @@ import (
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
+var ProviderSet = wire.NewSet(NewData, NewDB, NewGreeterRepo)
 
 // Data .
 type Data struct {
 	// TODO wrapped database client
+	db *gorm.DB
 }
 
 // NewData .
-func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
+func NewData(c *conf.Data, logger log.Logger, db *gorm.DB) (*Data, func(), error) {
 	cleanup := func() {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
-	return &Data{}, cleanup, nil
+	return &Data{db: db}, cleanup, nil
 }
 
 func NewDB(c *conf.Data) *gorm.DB {
-	db, err := gorm.Open(mysql.Open("user:dangerous@tcp(127.0.0.1:3306)/realworld?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(c.Database.Dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database, err:" + err.Error())
 	}
--- internal/data/data_test.go
+++ internal/data/data_test.go
@@ -1,12 +1,9 @@
 package data
 
 import (
-	"kratos-realworld/internal/conf"
 	"testing"
 )
 
 func TestNewDB(t *testing.T) {
-
-	db := NewDB(&conf.Data{})
-	t.Log(db)
+	NewDB(nil)
 }
