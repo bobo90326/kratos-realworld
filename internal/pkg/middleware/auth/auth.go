03da531197c178e3baae3bb37ce115a90352169d
Liu <bobo90326@163.com>
2024-01-30 00:48:56 +0800

    jwt
    jwt

--- internal/pkg/middleware/auth/auth.go
+++ internal/pkg/middleware/auth/auth.go
@@ -2,18 +2,44 @@ package auth
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/go-kratos/kratos/v2/middleware"
 	"github.com/go-kratos/kratos/v2/transport"
+	"github.com/golang-jwt/jwt"
 )
+
+func GenerateToken() string {
+
+}
 
 func JWTAuth() middleware.Middleware {
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
 			if tr, ok := transport.FromServerContext(ctx); ok {
-				str := tr.RequestHeader().Get("Authorization")
-				spew.Dump(str)
+				tokenString := tr.RequestHeader().Get("Authorization")
+				spew.Dump(tokenString)
+
+				token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+					// Don't forget to validate the alg is what you expect:
+					if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+						return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+					}
+					return "secret", nil
+				})
+
+				if err != nil {
+					return nil, err
+				}
+
+				if claims, ok := token.Claims.(jwt.MapClaims); ok {
+					spew.Dump(claims["userid"])
+				} else {
+					return nil, errors.New("invalid token")
+				}
+
 			}
 			return handler(ctx, req)
 		}
